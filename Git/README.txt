1de8f93601f5474ad8694b09649d0e1c97d57ef7
dnyaneshwarchambhare <96054549+dnyaneshwarchambhare@users.noreply.github.com>
2022-05-06 07:10:08 +0000

    Add files via upload

---
+++ Git/README.txt
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+read me files for js
+=======
+read me files here
+>>>>>>> js-assignments
---
+++ Git/login.js
@@ -0,0 +1,7 @@
+<script>
+let x = 5;
+let y = 6;
+let z = x + y;
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z;
+</script>
